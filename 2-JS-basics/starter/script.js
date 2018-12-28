08e21cab04619d6988a510b17a4e91a90d0bd82e
Conner Donohoo <donohoo13@gmail.com>
2018-12-28 13:06:56 -0700

    first commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,41 @@
+console.log('Hello World!!!');
+
+/*
+Variables and Datatypes and becoming familiar with JavaScript.
+*/
+
+// This is a single line comment.
+
+let firstName = 'Conner';
+let lastName = 'Donohoo';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName)
+
+let age = 23;
+console.log(`${fullName}, Age: ${age}`) /* template string */
+
+//Prompts and alerts will break node script.js
+// const girlfriend = prompt('What is your girlfriends name?');
+// alert(girlfriend + ' is an amazing woman.  Good choice!');
+// console.log(girlfriend);
+
+// ****************************************************************************
+
+
+let currentYear = 2018;
+let year = 365;
+let myAge = 23;
+let myBirthdayYear = 1995;
+let myBirthdayMonth = 'April';
+let myBirthdayDay = '5';
+
+console.log(`Hello, my name is ${fullName} and I am ${myAge} years old.  I was born in ${myBirthdayYear}
+ on the ${myBirthdayDay}th of ${myBirthdayMonth}.`)
+
+
+ // ****************************************************************************
+
+
+ var x, y;
+ x = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+ console.log(x)
